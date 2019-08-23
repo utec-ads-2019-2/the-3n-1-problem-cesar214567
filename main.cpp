a73574c6e84b2717f38ce3829592dcf6b7df169f
cesar214567 <cesar.madera@utec.edu.pe>
2019-08-22 19:33:45 -0500

    end of the problem

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
-
+#include <vector>
+#include <sstream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+unsigned int  algoritmo(unsigned int menor,unsigned int mayor){
+    unsigned int temporal,salida=0;
+    unsigned int sumador;
+    for (unsigned int i=menor;i<mayor+1;i++){
+        sumador=1;
+        temporal=i;
+        while (temporal!=1){
+            if (temporal%2==1){
+                temporal=temporal*3+1;
+                sumador+=1;
+            }
+            else{ 
+                temporal/=2;
+                sumador+=1;
+            }
+        }
+        if (sumador>salida){
+            
+            salida=sumador;
+        }
+        
+    }
+    
+    return salida;
+
+
+    
 }
+
+
+
+int main(){
+    unsigned int resultado=0;
+    unsigned int n,m;
+    while (scanf("%d %d",&n,&m)==2){
+        cout<<n<<" "<<m<<" ";
+        if (n>m){
+            resultado=algoritmo(m,n);
+            cout<<resultado<<endl;
+    }else{
+        resultado=algoritmo(n,m);
+        cout<<resultado<<endl;
+    }
+        }
+    
+        
+        
+    }
+    
+    
+    
